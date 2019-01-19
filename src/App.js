62995b6e7a3177c26958cf67cec5de475e897ba1
David Reese <dave@freshconsulting.com>
2019-01-18 16:41:39 -0800

    adding color palette, favicon, logo and enabling css grid

--- src/App.js
+++ src/App.js
@@ -4,21 +4,17 @@ import './App.scss';
 
 class App extends Component {
   render() {
+    let tempLogo = {
+      display: 'block',
+      margin: '0 auto',
+      marginTop: '45vh',
+      width: '300px'
+    };
+
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <img src={logo} className="App-logo" alt="logo" style={tempLogo} />
         </header>
       </div>
     );
