2015cf30e6c73be4a7e36940a6b570bac153c71d
Tyler Burkhardt <tylerburkhardt4@gmail.com>
2019-02-28 00:09:34 -0800

    make util more modulary

--- api/util.js
+++ api/util.js
@@ -3,22 +3,29 @@ const bcrypt = require('bcrypt');
 const knex = require('./services/knex');
 const secretKey = require('./config').SECRET_KEY;
 
-exports.generateToken = (payload) => {
+const generateToken = (payload) => {
   return jwt.sign(payload, secretKey);
 }
 
-exports.hashPassword = async (password) => {
+const hashPassword = async (password) => {
   return await bcrypt.hash(password, 10);
 }
 
-exports.checkPassword = async (password, hash) => {
+const checkPassword = async (password, hash) => {
   return await bcrypt.compare(password, hash);
 }
 
-exports.duplicateUserCheck = async (username) => {
+const duplicateUserCheck = async (username) => {
   const matchingUsers = await knex('users').where('username', username);
   
   if (matchingUsers.length > 0) {
     throw 'Username already exists';
   }
+}
+
+module.exports = {
+  generateToken: generateToken,
+  hashPassword: hashPassword,
+  checkPassword: checkPassword,
+  duplicateUserCheck: duplicateUserCheck,
 }
