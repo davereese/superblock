345c4c93f192cf2490eb1755f954ffeedb328e12
David Reese <dave@freshconsulting.com>
2019-02-28 21:37:04 -0800

    adding login request with axios and handling log in/log out

--- src/views/Login/Login.jsx
+++ src/views/Login/Login.jsx
@@ -1,18 +1,39 @@
 import React from 'react';
+import axios from 'axios';
 
 /*
-  This component displays the Login page view. It's set up as a class component
-  for now, because it may need to use state at some point in the future.
+  This component displays the Login page view and functionality.
  */
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      username: '',
+      password: '',
+    }
+  }
+
+  async loginUser() {
+    try {
+      const response = await axios.post('http://localhost:4000/api/login', {
+        username: this.state.username,
+        password: this.state.password
+      });
+      this.props.onLoginSuccess(response.data);
+      this.props.history.push('/');
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   render() {
+    const handleChange = (e) => {
+      this.setState({[e.target.id]: e.target.value});
+    }
+  
     const handleLogin = (e) => {
-      // do stuff
+      e.preventDefault();
+      this.loginUser();
     }
 
     return (
@@ -20,21 +41,27 @@ class Login extends React.Component {
         <div className="content login-container" role="main">
           <div className="login-container__form">
             <h1 className="header-lg">Log In</h1>
-            <label form="username">Username</label><br />
-            <input
-              type="text"
-              id="username"
-              autoComplete="off"
-            /><br />
-            <label form="password">Password</label><br />
-            <input
-              type="password"
-              id="password"
-            />
-            <button
-              type="submit"
-              onClick={handleLogin}
-            >Submit</button>
+            <form action="">
+              <label form="username">Username</label><br />
+              <input
+                type="text"
+                id="username"
+                autoComplete="off"
+                value={this.state.username}
+                onChange={handleChange}
+              /><br />
+              <label form="password">Password</label><br />
+              <input
+                type="password"
+                id="password"
+                value={this.state.password}
+                onChange={handleChange}
+              />
+              <button
+                type="submit"
+                onClick={handleLogin}
+              >Submit</button>
+            </form>
           </div>
         </div>
       </React.Fragment>
