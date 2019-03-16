05d508df37623bf627ffcc03a67becdcf6a074e6
David Reese <dave@freshconsulting.com>
2019-03-15 23:03:46 -0700

    adding the get block request to route

--- src/views/Editing/Editing.jsx
+++ src/views/Editing/Editing.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 import Editor from '../../components/Editor/Editor'
 import Hamburger from '../../components/Hamburger/Hamburger';
@@ -7,16 +8,40 @@ class Editing extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      content: '',
       language: '',
       isOpen: '',
       title: 'Block',
+      tags: [],
+    }
+
+    // query the block
+    if (props.match.params.id) {
+      this.queryBlock(props.match.params.id);
+    }
+  }
+
+  async queryBlock(blockId) {
+    try {
+      const response = await axios.get(`http://localhost:4000/api/blocks/${blockId}`, {
+        headers: {
+          'authorization': this.props.user.token
+        }
+      });
+      const data = response.data[0];
+      this.setState({
+        content: data.content,
+        title: data.title,
+        language: data.language,
+        tags: data.tags,
+      });
+    } catch (error) {
+      // handle error
+      console.error(error);
     }
   }
 
   render() {
-    // content will eventually hold what comes back from the api
-    let content;
-
     const handleInputChange = (e) => {
       const field = e.target.id;
       this.setState({title: `${e.target.value} block`});
@@ -27,9 +52,9 @@ class Editing extends React.Component {
       this.setState({isOpen: !this.state.isOpen});
     }
 
-    // const addTag = (e) => {
-    //   // add tag
-    // }
+    const addTag = (e) => {
+      // add tag
+    }
 
     return (
       <React.Fragment>
@@ -49,12 +74,21 @@ class Editing extends React.Component {
             <option value='sass'>SASS</option>
             <option value='html'>HTML</option>
           </select>
-          {/* Hide for unauthenticated users
-          <label htmlFor="newtag">Tag</label><br />
-          <div className="input-and-button">
-            <input type="text" id="newtag" />
-            <button type="button" onClick={addTag}>+</button>
-          </div> */}
+
+          {
+            this.props.user !== null ?
+              <>
+                <label htmlFor="newtag">Tag</label><br />
+                <div className="input-and-button">
+                  <input type="text" id="newtag" />
+                  <button type="button" onClick={addTag}>+</button>
+                </div>
+                <ul className="tagsList">
+                </ul>
+              </>
+            : null
+          }
+
         </div>
         <div
           className={`content ${this.state.isOpen}`}
@@ -68,7 +102,7 @@ class Editing extends React.Component {
           </button>
           <Editor
             blockTitle={this.state.title}
-            blockContent={content}
+            blockContent={this.state.content}
             language={this.state.language}
           />
         </div>
